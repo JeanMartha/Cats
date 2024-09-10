9141bce01873584675f7ab07d6518ae67645ea04
Jean Martha <tellojeanmartha@gmail.com>
2024-09-11 05:06:45 +0800

    First Commit

---
+++ ghe.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('click-me-button');
+    
+    button.addEventListener('click', () => {
+       
+        window.location.href = 'ghe2.html'; 
+    });
+});
+
